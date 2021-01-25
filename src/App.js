3662915b9793a9043cb37a081af6ecc0f854a8ee
Korey Nichols <koreynichols1@gmail.com>
2021-01-24 22:21:19 -0800

    got movie list set up to list movies from useState

--- src/App.js
+++ src/App.js
@@ -1,13 +1,21 @@
+import React, { useState } from 'react';
 import './App.css';
 
 function App() {
+
+  const [movies, setMovie] = useState(['taco', 'taco']);
+
   return (
     <div className="App">
       <header className="App-header">
         <h1>Movie Rater</h1>
       </header>
       <div className="layout">
-          <div>Movie list</div>
+          <div>
+            { movies.map( movie => {
+              return <h2>{movie}</h2>
+            })}
+          </div>
           <div>Movie details</div>
         </div>
     </div>
