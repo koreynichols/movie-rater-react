0fabcc6ced70596231835c685de3b1c48887aab1
Korey Nichols <koreynichols1@gmail.com>
2021-01-25 14:37:41 -0800

    working on calling backend api. need to set up cors settings

--- src/App.js
+++ src/App.js
@@ -1,9 +1,22 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import './App.css';
 
 function App() {
 
-  const [movies, setMovie] = useState(['taco', 'taco']);
+  const [movies, setMovie] = useState([]);
+
+  useEffect(()=>{
+    fetch("http://127.0.0.1:8000/api/movies/", {
+      method: 'GET',
+      headers: {
+        'Content-Type': 'application/json',
+        'Authorization': 'Token'
+      }
+    })
+    .then( resp => resp.json())
+    .then( resp => setMovie(resp))
+    .catch( error => console.log(error))
+  }, [])
 
   return (
     <div className="App">
