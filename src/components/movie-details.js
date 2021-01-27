8e57725cec282ce6a54df70c20e4c95796657b30
Korey Nichols <koreynichols1@gmail.com>
2021-01-26 21:48:22 -0800

    added stars to show what the movie rating is and how many reviews it has

--- src/components/movie-details.js
+++ src/components/movie-details.js
@@ -3,13 +3,21 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faStar } from '@fortawesome/free-solid-svg-icons';
 
 function MovieDetails(props){
+
+    const mov = props.movie;
+
     return (
         <div>
-            { props.movie ? (
+            { mov ? (
                 <div>
-                    <h1>{props.movie.title}</h1>
-                    <p>{props.movie.description}</p>
-                    <FontAwesomeIcon icon={faStar} />
+                    <h1>{mov.title}</h1>
+                    <p>{mov.description}</p>
+                    <FontAwesomeIcon icon={faStar} className={mov.avg_rating > 0 ? 'orange': ''}/>
+                    <FontAwesomeIcon icon={faStar} className={mov.avg_rating > 1 ? 'orange': ''}/>
+                    <FontAwesomeIcon icon={faStar} className={mov.avg_rating > 2 ? 'orange': ''}/>
+                    <FontAwesomeIcon icon={faStar} className={mov.avg_rating > 3 ? 'orange': ''}/>
+                    <FontAwesomeIcon icon={faStar} className={mov.avg_rating > 4 ? 'orange': ''}/>
+                    ({mov.no_of_ratings})
                 </div>
             ) : null }
         </div>
