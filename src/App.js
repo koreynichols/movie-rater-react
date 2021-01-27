2a1ce4978f8e0c29d04d77dbcc01d16c6ca2d468
Korey Nichols <koreynichols1@gmail.com>
2021-01-26 16:56:21 -0800

    set up click event when you click on a movie title. need to create display for movie information

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import MovieList from './components/movie-list';
 function App() {
 
   const [movies, setMovie] = useState([]);
+  const [selectedMovie, setSelectedMovie] = useState(null);
 
   useEffect(()=>{
     fetch("http://127.0.0.1:8000/api/movies/", {
@@ -18,13 +19,17 @@ function App() {
     .catch( error => console.log(error))
   }, [])
 
+  const movieClicked = movie => {
+    setSelectedMovie(movie.title);
+  }
+
   return (
     <div className="App">
       <header className="App-header">
         <h1>Movie Rater</h1>
       </header>
       <div className="layout">
-          <MovieList movies={movies} />
+          <MovieList movies={movies} movieClicked={movieClicked} />
           <div>Movie details</div>
         </div>
     </div>
