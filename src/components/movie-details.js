d3bd95181a74c1f9246d437deea36cea61395445
Korey Nichols <koreynichols1@gmail.com>
2021-01-27 16:10:33 -0800

    working on making a rating submit system. currently broken, needs more work

--- src/components/movie-details.js
+++ src/components/movie-details.js
@@ -1,13 +1,15 @@
-import React from 'react';
+import React, {useState } from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faStar } from '@fortawesome/free-solid-svg-icons';
 
 function MovieDetails(props){
 
+    const [ highlighted, setHighlighted] = useState(-1);
+
     const mov = props.movie;
 
     return (
-        <div>
+        <React.Fragment>
             { mov ? (
                 <div>
                     <h1>{mov.title}</h1>
@@ -18,9 +20,18 @@ function MovieDetails(props){
                     <FontAwesomeIcon icon={faStar} className={mov.avg_rating > 3 ? 'orange': ''}/>
                     <FontAwesomeIcon icon={faStar} className={mov.avg_rating > 4 ? 'orange': ''}/>
                     ({mov.no_of_ratings})
+                    <div className="rate-container">
+                        <h2>Rate it</h2>
+                        { [...Array(5)].map( (e, i) => {
+                            return <FontAwesomeIcon key={i} icon={faStar} className={highlighted > i - 1 ? 'purple': ''}
+                                onMouseEnter={setHighlighted(i)}
+                                onMouseLeave={setHighlighted(-1)}
+                            />
+                        })}
+                    </div>
                 </div>
             ) : null }
-        </div>
+        </React.Fragment>
     )
 }
 
