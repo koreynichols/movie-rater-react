6dfadda779cf920ba382340673b1be6733fae304
Korey Nichols <koreynichols1@gmail.com>
2021-02-02 16:26:47 -0800

    added api call to update movie information

---
+++ src/api-service.js
@@ -0,0 +1,12 @@
+export class API {
+    static updateMovie(mov_id, body) {
+        return fetch(`http://127.0.0.1:8000/api/movies/${mov_id}/`, {
+            method: 'PUT',
+            headers: {
+                'Content-Type': 'application/json',
+                'Authorization': process.env.REACT_APP_TOKEN
+            },
+            body: JSON.stringify( body )
+        }).then( resp => resp.json())
+    }
+}
