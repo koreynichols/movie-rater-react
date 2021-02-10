7becf6ce375009530fc66ce64cb70e70d7aa0ca6
Korey Nichols <koreynichols1@gmail.com>
2021-02-10 15:34:08 -0800

    login returns token for user

--- src/components/auth.js
+++ src/components/auth.js
@@ -1,8 +1,29 @@
-import React from 'react';
+import React, {useState} from 'react';
+import { API } from '../api-service';
 
 function Auth(){
+
+    const [ username, setUsername ] = useState('');
+    const [ password, setPassword ] = useState('');
+
+    const loginClicked = () => {
+        API.loginUser({username, password})
+            .then( resp => console.log(resp.token))
+            .catch( error => console.log(error))
+    }
+
     return (
-        <h1>AUTH</h1>
+        <div>
+            <label htmlFor="username">Username</label><br/>
+            <input id="username" type="text" placeholder="username" value={username}
+                onChange={ evt=> setUsername(evt.target.value)}
+            /><br/>
+            <label htmlFor="password">Password</label><br/>
+            <input id="password" type="password" placeholder="password" value={password}
+                onChange={ evt=> setPassword(evt.target.value)}
+            /><br/>
+            <button onClick={loginClicked}>Login</button>
+        </div>    
     )
 }
 
